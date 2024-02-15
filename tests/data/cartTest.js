05bb2509598945d1bfbd2a77edd370cee12a241f
sara.ghasemimehr <sara.qasemimehr@gmail.com>
2024-02-16 00:27:14 +0330

    16d

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -6,17 +6,22 @@ describe('test suite: addToCard', ()=>{
 
   spyOn(localStorage, 'getItem').and.callFake(() => {
     return JSON.stringify([{
-      productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6 ',
+      productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
       quantity: 1,
       deliveryOptionId: '1'
     }]);
   }); 
   loadFromStorage(); 
 
-  addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6 ');
+  addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
   expect(cart.length).toEqual(1); 
   expect(localStorage.setItem).toHaveBeenCalledTimes(1);
-  expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6 ');
+  expect(localStorage.setItem).toHaveBeenCalledWith('cart',JSON.stringify([{
+    productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+    deliveryOptionId: '1'
+  }]));
+  expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
   expect(cart[0].quantity).toEqual(2); 
 
  });
@@ -29,11 +34,16 @@ describe('test suite: addToCard', ()=>{
   }); 
   loadFromStorage(); 
   
-   addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6 ');
+   addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
    expect(cart.length).toEqual(1); 
    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
-   expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6 ');
+   expect(localStorage.setItem).toHaveBeenCalledWith('cart',JSON.stringify([{
+    productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 1,
+  }]));
+   expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
    expect(cart[0].quantity).toEqual(1); 
+ 
  });
 
 });
