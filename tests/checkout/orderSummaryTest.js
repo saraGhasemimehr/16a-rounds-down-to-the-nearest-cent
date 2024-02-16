0d9efc954598af14d7f47206764fb2277ee1e428
sara.ghasemimehr <sara.qasemimehr@gmail.com>
2024-02-16 23:52:41 +0330

    16h

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -49,11 +49,18 @@ describe('test suite: renderOrderSummary', () =>{
     expect(
      document.querySelector(`.js-product-quantity-${productId2}`).innerText).toContain('Quantity: 1');  
 
-     expect(
-      document.querySelector(`.js-product-name-${productId1}`).innerText).toEqual('Black and Gray Athletic Cotton Socks - 6 Pairs');
+    expect(
+     document.querySelector(`.js-product-name-${productId1}`).innerText).toEqual('Black and Gray Athletic Cotton Socks - 6 Pairs');
 
-      expect(
-        document.querySelector(`.js-product-name-${productId2}`).innerText).toEqual('Intermediate Size Basketball');
+    expect(
+    document.querySelector(`.js-product-name-${productId2}`).innerText).toEqual('Intermediate Size Basketball');
+
+    
+    expect(
+    document.querySelector(`.js-product-price-${productId1}`).innerText).toEqual('$10.90');
+
+    expect(
+    document.querySelector(`.js-product-price-${productId2}`).innerText).toEqual('$20.95');
      
 
   });
@@ -73,9 +80,14 @@ describe('test suite: renderOrderSummary', () =>{
     expect(
       document.querySelector(`.js-cart-item-container-${productId2}`)
       ).not.toEqual(null); 
+
     expect(
       document.querySelector(`.js-product-name-${productId2}`).innerText
     ).toEqual('Intermediate Size Basketball');
+    
+    expect(
+      document.querySelector(`.js-product-price-${productId2}`).innerText
+    ).toEqual('$20.95');
 
     expect(cart.length).toEqual(1);
     expect(cart[0].productId).toEqual(productId2);
